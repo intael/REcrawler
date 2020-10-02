1b59ec1f67c33da0ad0b1f477f59c9612a56697f
intael <carlosvalde9@gmail.com>
2020-10-02 18:23:02 +0300

    Adding constructor checks for PriceRange

--- src/main/java/webcrawling/specification/PriceRange.java
+++ src/main/java/webcrawling/specification/PriceRange.java
@@ -1,13 +1,35 @@
 package webcrawling.specification;
 
+import org.jetbrains.annotations.NotNull;
 import realestate.measures.Price;
 
 public class PriceRange {
-  private Price lowerBound;
-  private Price upperBound;
+  public Price getLowerBound() {
+    return lowerBound;
+  }
 
-  public PriceRange(Price lowerBound, Price upperBound) {
+  public Price getUpperBound() {
+    return upperBound;
+  }
+
+  private final Price lowerBound;
+  private final Price upperBound;
+
+  public PriceRange(@NotNull Price lowerBound, @NotNull Price upperBound) {
+    if(lowerBound.getAmount() > upperBound.getAmount()){
+      throw new IllegalArgumentException("Invalid Price Range: The lower bound price is higher than the upper bound price.");
+    }
+    if(!lowerBound.getCurrency().equals(upperBound.getCurrency()))
+      throw new IllegalArgumentException("Invalid Price Range: The upper and lower bound price currencies do not match.");
     this.lowerBound = lowerBound;
     this.upperBound = upperBound;
+  }
+
+  @Override
+  public String toString() {
+    return "PriceRange{" +
+        "lowerBound=" + lowerBound +
+        ", upperBound=" + upperBound +
+        '}';
   }
 }
