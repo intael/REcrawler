995d896f2cb45cb8acd50f75cddc448a9ddae6ec
intael <carlosvalde9@gmail.com>
2020-10-04 11:20:28 +0300

    Adding first basic SearchRealEstateUrlBuilder tests

---
+++ src/test/java/webcrawlers/fotocasa/SearchRealEstateUrlBuilderTest.java
@@ -0,0 +1,49 @@
+package webcrawlers.fotocasa;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Locale;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import realestate.measures.Price;
+import webcrawlers.fotocasa.specification.FotocasaContract;
+import webcrawlers.fotocasa.specification.Home;
+import webcrawling.specification.PriceRange;
+
+public class SearchRealEstateUrlBuilderTest {
+
+  SearchRealEstateUrlBuilder instanceWithoutOptionals;
+  SearchRealEstateUrlBuilder instanceWithAllOptionals;
+
+  @BeforeEach
+  public void setUp() {
+    instanceWithoutOptionals =
+        new SearchRealEstateUrlBuilder.Builder("barcelona", new Locale("en")).build();
+    Price lowerBound = new Price(800, "EUR");
+    Price upperBound = new Price(2000, "EUR");
+    PriceRange somePriceRange = new PriceRange(lowerBound, upperBound);
+    instanceWithAllOptionals =
+        new SearchRealEstateUrlBuilder.Builder("barcelona", new Locale("en"))
+            .withContract(FotocasaContract.RENTAL)
+            .withPriceRange(somePriceRange)
+            .atZone("Sants")
+            .withHome(Home.FLATS)
+            .withMinimumRestRooms(2)
+            .withMinimumRooms(3)
+            .build();
+  }
+
+  @Test
+  public void buildUrlWithoutOptionalsCreatesExpectedUrl() {
+    assertEquals(
+        "https://www.fotocasa.es/en/buy/homes/barcelona/all-zones/l",
+        instanceWithoutOptionals.buildUrl());
+  }
+
+  @Test
+  public void buildUrlWithAllOptionalsCreatesExpectedUrl() {
+    assertEquals(
+        "https://www.fotocasa.es/en/rental/flats/barcelona/sants/l?minPrice=800&maxPrice=2000&minRooms=3&minBathrooms=2",
+        instanceWithAllOptionals.buildUrl());
+  }
+}
