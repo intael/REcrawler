c14943c14b15c675e5f88340077bb1062b3b495b
intael <carlosvalde9@gmail.com>
2020-10-06 06:57:19 +0300

    Changing signature of SiteCollector interface and MVP of FotocasaSiteCollector

--- src/main/java/webcrawlers/fotocasa/FotocasaSiteCollector.java
+++ src/main/java/webcrawlers/fotocasa/FotocasaSiteCollector.java
@@ -3,6 +3,8 @@ package webcrawlers.fotocasa;
 import java.io.IOException;
 import java.net.Proxy;
 import java.util.Map;
+import java.util.Optional;
+import org.jetbrains.annotations.NotNull;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -12,29 +14,59 @@ import webcrawling.SiteCollector;
 
 public class FotocasaSiteCollector implements SiteCollector {
 
-  private String userAgent;
-  private Map<String, String> cookies;
+  private final String userAgent;
+  private final Map<String, String> cookies;
+
+  public FotocasaSiteCollector(
+      @NotNull String userAgent,
+      Map<String, String> cookies,
+      Proxy proxy,
+      int timeout,
+      int max_retries) {
+    this.userAgent = userAgent;
+    this.cookies = cookies;
+    this.proxy = proxy;
+    this.timeout = timeout;
+    this.max_retries = max_retries;
+  }
+
+  public FotocasaSiteCollector(@NotNull String userAgent) {
+    this(userAgent, null, null, 30_000, 3);
+  }
+
+  public Proxy getProxy() {
+    return proxy;
+  }
+
+  public void setProxy(@NotNull Proxy proxy) {
+    this.proxy = proxy;
+  }
+
   private Proxy proxy;
-  private static int TIMEOUT = 5_000;
-  private static int MAX_RETRIES = 5;
+  private final int timeout; // in milliseconds
+  private final int max_retries;
   private final Logger logger = LoggerFactory.getLogger(FotocasaSiteCollector.class);
 
-  public Document collect(String url) {
-    Connection conn = Jsoup.connect(url);
-    conn = addCookies(conn);
+  // TODO: Add stateful usage statistics (eg, flush method)
+  public Optional<Document> collect(String url) {
+    Connection conn = Jsoup.connect(url).userAgent(userAgent).timeout(timeout);
+    if (proxy != null) conn = conn.proxy(proxy);
+    if (cookies != null) conn = addCookies(conn);
     int attempts = 0;
-    while(attempts <= MAX_RETRIES){
-      try{
-        return conn.get();
-      }
-      catch (IOException requestError){
+    while (attempts < max_retries) {
+      try {
+        return Optional.of(conn.get());
+      } catch (IOException requestError) {
         attempts++;
+        // TODO: Make logging clearer and more informative
         logger.error("Attempt: " + attempts + ".\nTrace: " + requestError.toString());
       }
     }
+    return Optional.empty();
   }
-  private Connection addCookies(Connection connection){
-    for(String cookie: cookies.keySet()){
+
+  private Connection addCookies(Connection connection) {
+    for (String cookie : cookies.keySet()) {
       connection = connection.cookie(cookie, cookies.get(cookie));
     }
     return connection;
--- src/main/java/webcrawling/SiteCollector.java
+++ src/main/java/webcrawling/SiteCollector.java
@@ -1,9 +1,11 @@
 package webcrawling;
 
 import java.io.IOException;
+import java.util.Optional;
 import org.jsoup.nodes.Document;
 
 public interface SiteCollector {
 
-  Document collect(String url) throws IOException;
+  Optional<Document> collect(String url) throws IOException;
+
 }
