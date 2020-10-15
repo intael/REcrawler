8c506df237029a038db1b076542b7346e806b1f3
intael <carlosvalde9@gmail.com>
2020-10-15 07:00:00 +0300

    Add another house page sample for FotocasaListingHtmlParser unit tests

---
+++ src/main/java/webcrawlers/fotocasa/FotocasaRealEstateRepository.java
@@ -0,0 +1,9 @@
+package webcrawlers.fotocasa;
+
+import realestate.RealEstate;
+import webcrawling.RealEstateRepository;
+
+public class FotocasaRealEstateRepository implements RealEstateRepository {
+  @Override
+  public void save(RealEstate realEstate) {}
+}
