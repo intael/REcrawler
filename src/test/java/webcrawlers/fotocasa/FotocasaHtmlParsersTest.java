985218007b0c7fce5dd9284248eeb7b8a884e21f
intael <carlosvalde9@gmail.com>
2020-10-15 07:00:00 +0300

    Add another house page sample for FotocasaListingHtmlParser unit tests

--- src/test/java/webcrawlers/fotocasa/FotocasaHtmlParsersTest.java
+++ src/test/java/webcrawlers/fotocasa/FotocasaHtmlParsersTest.java
@@ -3,7 +3,10 @@ package webcrawlers.fotocasa;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import org.jetbrains.annotations.NotNull;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.junit.Assert;
@@ -18,19 +21,25 @@ class FotocasaHtmlParsersTest {
 
   private static final String LISTINGS_PAGE_SAMPLE_FILENAME =
       "listings-page-sample-2020-10-07.html";
-  private static final String HOUSE_PAGE_SAMPLE_FILENAME = "house-page-sample-2020-10-12.html";
+  private static final String HOUSE_PAGE_SAMPLE_1_FILENAME = "house-page-sample-2020-10-12.html";
+  private static final String HOUSE_PAGE_SAMPLE_2_FILENAME = "house-page-sample-2020-10-15.html";
   private Document listingsSearchResultPageSample;
-  private Document housePageSample;
+  private final Set<Document> housePageSamples = new HashSet<>();
   private static final String FOTOCASA_PAGINATION_SEARCH_RESULTS_CSS_CLASS_NAME =
       "a.sui-LinkBasic.sui-PaginationBasic-link";
   private static final String FOTOCASA_LISTINGS_SEARCH_RESULTS_CSS_CLASS_NAME = "a.re-Card-link";
 
   @BeforeEach
   void setUp() throws IOException {
-    File htmlSampleFile = TestFilesUtils.getHtmlSampleAsFile(LISTINGS_PAGE_SAMPLE_FILENAME);
-    this.listingsSearchResultPageSample = Jsoup.parse(htmlSampleFile, null);
-    htmlSampleFile = TestFilesUtils.getHtmlSampleAsFile(HOUSE_PAGE_SAMPLE_FILENAME);
-    this.housePageSample = Jsoup.parse(htmlSampleFile, null);
+    this.listingsSearchResultPageSample = readAndParseHtmlFile(LISTINGS_PAGE_SAMPLE_FILENAME);
+    housePageSamples.add(readAndParseHtmlFile(HOUSE_PAGE_SAMPLE_1_FILENAME));
+    housePageSamples.add(readAndParseHtmlFile(HOUSE_PAGE_SAMPLE_2_FILENAME));
+  }
+
+  @NotNull
+  private Document readAndParseHtmlFile(String fileName) throws IOException {
+    File htmlFile = TestFilesUtils.getHtmlSampleAsFile(fileName);
+    return Jsoup.parse(htmlFile, null);
   }
 
   @Test
@@ -65,9 +74,13 @@ class FotocasaHtmlParsersTest {
   @Test
   void realEstateParserReturnsRealEstateGivenAFotocasaHousePage() {
     HtmlParser<RealEstate> searchListingsParser = new FotocasaListingHtmlParser();
-    List<RealEstate> realEstates = searchListingsParser.parse(housePageSample);
-    Assert.assertEquals(1, realEstates.size());
-    FotocasaHome parsedHouse = (FotocasaHome) realEstates.get(0);
-    Assert.assertNotNull(parsedHouse);
+    for(Document sample: housePageSamples){
+      List<RealEstate> realEstates = searchListingsParser.parse(sample);
+      Assert.assertEquals(1, realEstates.size());
+      FotocasaHome parsedHouse = (FotocasaHome) realEstates.get(0);
+      System.out.println(parsedHouse);
+      Assert.assertNotNull(parsedHouse);
+    }
+
   }
 }
