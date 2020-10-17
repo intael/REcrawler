92ccd9274075fa480824c683b4346339747f157a
intael <carlosvalde9@gmail.com>
2020-10-17 13:21:09 +0300

    Add overloaded method to SiteCollector so it is possible to specify retries on each collect call

--- src/main/java/webcrawlers/fotocasa/FotocasaSiteCollector.java
+++ src/main/java/webcrawlers/fotocasa/FotocasaSiteCollector.java
@@ -12,22 +12,27 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import webcrawling.SiteCollector;
 
+// TODO: Add stateful usage statistics (eg, flush method)
 public class FotocasaSiteCollector implements SiteCollector {
 
   private final String userAgent;
   private final Map<String, String> cookies;
+  private Proxy proxy;
+  private final int timeout; // in milliseconds
+  private final int maxRetries;
+  private final Logger logger = LoggerFactory.getLogger(FotocasaSiteCollector.class);
 
   public FotocasaSiteCollector(
       @NotNull String userAgent,
       Map<String, String> cookies,
       Proxy proxy,
       int timeout,
-      int max_retries) {
+      int maxRetries) {
     this.userAgent = userAgent;
     this.cookies = cookies;
     this.proxy = proxy;
     this.timeout = timeout;
-    this.max_retries = max_retries;
+    this.maxRetries = maxRetries;
   }
 
   public FotocasaSiteCollector(@NotNull String userAgent) {
@@ -42,27 +47,30 @@ public class FotocasaSiteCollector implements SiteCollector {
     this.proxy = proxy;
   }
 
-  private Proxy proxy;
-  private final int timeout; // in milliseconds
-  private final int max_retries;
-  private final Logger logger = LoggerFactory.getLogger(FotocasaSiteCollector.class);
-
-  // TODO: Add stateful usage statistics (eg, flush method)
-  public Optional<Document> collect(String url) {
+  public Optional<Document> collect(String url, int maxRetries) {
     Connection conn = Jsoup.connect(url).userAgent(userAgent).timeout(timeout);
     if (proxy != null) conn = conn.proxy(proxy);
     if (cookies != null) conn = addCookies(conn);
     int attempts = 0;
-    while (attempts < max_retries) {
+    while (attempts < maxRetries) {
       try {
         return Optional.of(conn.get());
       } catch (IOException requestError) {
         attempts++;
         // TODO: Make logging clearer and more informative
-        logger.error("Attempt: " + attempts + ".\nTrace: " + requestError.toString());
+        logger.error(
+            String.format(
+                "Failure with proxy: %s. Attempt: %s. Trace: %s",
+                this.proxy == null ? "Not using a proxy." : this.proxy.toString(),
+                attempts,
+                requestError.toString()));
       }
     }
     return Optional.empty();
+  }
+
+  public Optional<Document> collect(String url) {
+    return collect(url, maxRetries);
   }
 
   private Connection addCookies(Connection connection) {
