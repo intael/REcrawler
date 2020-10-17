b1904bd02250d8c5aa871934f04272c02f47e57f
intael <carlosvalde9@gmail.com>
2020-10-17 13:18:53 +0300

    Transform relative urls into absolute ones when needed FotocasaFetchUrlsHtmlParser

--- src/main/java/webcrawlers/fotocasa/FotocasaFetchUrlsHtmlParser.java
+++ src/main/java/webcrawlers/fotocasa/FotocasaFetchUrlsHtmlParser.java
@@ -19,7 +19,7 @@ public class FotocasaFetchUrlsHtmlParser implements HtmlParser<URL> {
   public List<URL> parse(@NotNull Document document) {
     Elements linkTags = document.select(cssClassName);
     return linkTags.stream()
-        .map((element -> element.attr(HREF_ATTRIBUTE)))
+        .map((element -> element.absUrl(HREF_ATTRIBUTE)))
         .map(HtmlParser::deserializeUrl)
         .distinct()
         .collect(Collectors.toList());
